fc4f8e070f95fda8b4fae5990695ba29b7c2461d
mahamudul hasan seber <sebermh@gmail.com>
2026-02-01 23:02:25 +0600

    Default Parameter Code

---
+++ default-parameter.js
@@ -0,0 +1,11 @@
+function sum(num1, num2 = 0){
+    const total = num1 + num2;
+    // console.log(num1, num2, total);
+}
+sum(10, 40)
+
+function multiply(num1 = 2, num2 = 10){
+    const result = num1 * num2;
+    console.log(result)
+}
+multiply()
