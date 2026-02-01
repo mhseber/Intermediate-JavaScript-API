044c2bd2c2e39493fe77e859ce681ae8f3e49dcd
mahamudul hasan seber <sebermh@gmail.com>
2026-02-01 23:21:21 +0600

    Template-String

---
+++ template-string.js
@@ -0,0 +1,17 @@
+const fb = `Follow me on Facebook: https://www.facebook.com/mhseber
+and Twitter: https://www.twitter.com/mhseber
+and Instagram: https://www.instagram.com/mhseber`
+// console.log(fb);
+
+
+function code (rang1, rang2){
+    const fullCode = `rang of ${rang1+3} and ${rang2}`;
+    // console.log(fullCode);
+}
+code(5, 10);
+
+
+
+const price = 15000;
+const discountedPrice = `Discounted price is after 30% discount ${price * 0.08}`
+console.log(discountedPrice);
