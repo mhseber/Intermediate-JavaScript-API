78bb836296b207024bf8eb1f256e272d9d94f768
mahamudul hasan seber <sebermh@gmail.com>
2026-02-02 00:48:07 +0600

    Keys, values, delete, etc

---
+++ entres.js
@@ -0,0 +1,10 @@
+const employee = {
+    name: 'John Doe',
+    age: 30,
+    position: 'Software Engineer'
+};
+
+const keys = Object.keys(employee);
+const values = Object.values(employee);
+console.log('Keys:', keys);
+console.log('Values:', values);
