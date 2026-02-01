c124d7b25a489e03d10dfbdfa8aeb0c89559599e
mahamudul hasan seber <sebermh@gmail.com>
2026-02-02 00:42:08 +0600

    Object and Array Destructuring

---
+++ array-destracturing.js
@@ -0,0 +1,8 @@
+const {price, brand} = { name: 'Laptop', price: 32000, brand: 'Lenovo' };
+
+// console.log('Price:', price);
+// console.log('Brand:', brand);
+
+const numbers = [45, 23, 89, 67, 34];
+const [first, second] = numbers;
+console.log('First:', first);
