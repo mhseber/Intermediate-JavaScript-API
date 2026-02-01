e8014269d6319c2a218c1e458d66c4bdd6d6bd45
mahamudul hasan seber <sebermh@gmail.com>
2026-02-02 00:27:42 +0600

    Spread Operator

---
+++ spread.js
@@ -0,0 +1,9 @@
+const numbers = [87, 20, 22, 40 ,39, 555, 90];
+// console.log('Numbers:', numbers);
+// console.log('Numbers:', ...numbers);
+
+
+const max = Math.max(1121,45,3333,888,23,99);
+const max2 = Math.max(...numbers);
+console.log('Max number:',max);
+console.log('Max number from array:', max2);
